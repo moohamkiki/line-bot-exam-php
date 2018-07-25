b4cf857ba067787b50a972f1d96c0b29c1ab57f2
moohamkiki <41609683+moohamkiki@users.noreply.github.com>
2018-07-25 11:09:06 +0700

    Update testone.php

--- testone.php
+++ testone.php
@@ -11,7 +11,7 @@
     //รับข้อความจากผู้ใช้
     $message = $arrayJson['events'][0]['message']['text'];
 #ตัวอย่าง Message Type "Text"
-    if($message == "cause" || "num" || "Num" || "Help" || "help"){
+    if($message == "cause" || $message == "num" ||$message == "Num" || $message == "Help" ||$message == "help"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "1EN - Unallocated (unassigned) number.\n2EN - No route to specified transit network (national use).\n3 - No route to destination.\nCause No. 4 - send special information tone.\nCause No. 5 - misdialed trunk prefix (national use).\nCause No. 6 - channel unacceptable.\nCause No. 7 - call awarded. being delivered in an established channel.\nCause No. 8 - preemption.\nCause No. 9 - preemption - circuit reserved for reuse\n";
