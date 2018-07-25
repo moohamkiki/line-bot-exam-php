3aded01ec89fef345740a11141dac30dddbd6ecd
moohamkiki <41609683+moohamkiki@users.noreply.github.com>
2018-07-25 09:30:53 +0700

    Update nub.php

--- nub.php
+++ nub.php
@@ -1,27 +1,12 @@
-
 <?php
-
-// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
-
-include ('line-bot-api/php/line-bot.php');
-
+require "vendor/autoload.php";
+$access_token = 'bD/LUWMz3ny45E5H+FAvJX8YRaqZ70lE1m7GLeIPlOfG5hi69i7Bnkti2S+2FlHtyqfWtwlrPvH/deA8wxyye8Z96Cx+z1qerbRumV/EqNvY8mW/RfQXrbn7Volv3ZxBEqAftdYq4ZWuvQFXVmCEAAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'b85b195d286c2f64822206d0787450b8';
-$access_token  = 'bD/LUWMz3ny45E5H+FAvJX8YRaqZ70lE1m7GLeIPlOfG5hi69i7Bnkti2S+2FlHtyqfWtwlrPvH/deA8wxyye8Z96Cx+z1qerbRumV/EqNvY8mW/RfQXrbn7Volv3ZxBEqAftdYq4ZWuvQFXVmCEAAdB04t89/1O/w1cDnyilFU=';
+$idPush = 'xxxxxx'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
 
-$bot = new BOT_API($channelSecret, $access_token);
-	
-if (!empty($bot->isEvents)) {
-		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
-
-	if ($bot->isSuccess()) {
-		echo 'Succeeded!';
-		exit();
-	}
-
-	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-	exit();
-
-}
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
